7632135e2cf69e117773ccc0e28ef2d5c8859623
Sean <35079464+Hexpixel@users.noreply.github.com>
2018-01-31 18:16:33 -0500

    Update testdata.py

--- testdata.py
+++ testdata.py
@@ -12,8 +12,8 @@ import time
 WHITE = (255, 255, 255),
 BLACK = (0, 0, 0),
 RED = (255, 0, 0),
-GREEN = (0, 255, 0),
-BLUE = (0, 0, 139)
+GREEN = (63, 163, 47),
+BLUE = (131, 177, 255)
 
 # expand this value to make terrain less "pointy"
 frequency = 20
