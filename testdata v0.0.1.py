8c9e73b0416fc8cf52e727185ba010ba68b62a14
Sean <35079464+Hexpixel@users.noreply.github.com>
2018-02-11 14:03:28 -0500

    Update testdata v0.0.1.py

--- testdata v0.0.1.py
+++ testdata v0.0.1.py
@@ -1,25 +1,25 @@
 """
-terabit game ver. 0.0.1
 
->>MIT License
+terabit v0.0.1
 
 includes:
+>>MIT License
 
-procedural terrain generation
-scripted by: TheGreatRambler and Terapixel :D
+description:
+A two-dimensional terrain and sandbox based game written by Terapixel (Hexpixel) and TheGreatRambler.
+
 """
 
 # imports
 
 import pygame
-import sys
 import math
 from opensimplex import OpenSimplex
 import random
 
 
 
-# defines some colors
+# stores some colors in different variables allowing easier access to colors.
 
 WHITE = (255, 255, 255),
 BLACK = (0, 0, 0),
@@ -72,11 +72,11 @@ game_display = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 # sets the game window's left hand upper corner caption
 
-pygame.display.set_caption('terabit ver. 0.0.1')
+pygame.display.set_caption('terabit v0.0.1')
 
 # frames per second - increase this value to have smoother game play... note to TheGreatRambler: wouldn't advise this for Windows Vista users kek :P
 
-frames_per_second = 120
+frames_per_second = 500
 
 # player x and y
 
@@ -93,26 +93,31 @@ def returnarrayindex(xvalue, yvalue):
     y = yvalue * 2 if yvalue >= 0 else yvalue * -2 - 1
     number = (x * x + x + y) if (x >= y) else (y * y + x)
     return number
-    pygame.display.update()
+
 
 def gettopcorner():
     return [playerx - (SCREEN_WIDTH / 2) / BLOCK_WIDTH, playery - (SCREEN_HEIGHT / 2) / BLOCK_HEIGHT]
-    pygame.display.update()
-    
+
+
 def drawsquare(x, y, texture):
     topcornerdata = gettopcorner()
     dataforsquare = ((x - topcornerdata[0]) * BLOCK_WIDTH, (y - topcornerdata[1]) * BLOCK_HEIGHT, BLOCK_WIDTH, BLOCK_HEIGHT)
     width = 0
     pygame.draw.rect(game_display, texture, dataforsquare, width)
-    pygame.display.update()
-    
-def draw():
-    pygame.display.update()
+
+
+#def draw():
+    #pygame.display.update()
+    #timer.tick(frames_per_second)
+
+def drawStuff():
+    pygame.display.flip()
     timer.tick(frames_per_second)
+    game_display.fill(BLUE)
 
 
 
-class player:
+class player(pygame.sprite.Sprite):
     def __init__(self, velocity, maxJumpRange):
         self.velocity = velocity
         self.maxJumpRange = maxJumpRange
@@ -124,29 +129,33 @@ class player:
         self.jumping = False
         self.jumpCounter = 0
         self.falling = True
-        
+
     # player controls:
-    
+
     def go_up(self):
         uk = pygame.key.get_pressed()
-        if uk[pygame.K_w]:
+        if uk[pygame.K_SPACE]:
             self.jumping = True
             self.jumpCounter = 0
-            
+
     #def go_down(self):
        #dk = pygame.key.get_pressed()
-       #if dk[pygame.K_s]:     
-    
+       #if dk[pygame.K_s]:
+
     def go_right(self):
         rk = pygame.key.get_pressed()
-        if rk = [pygame.K_d]:
+        if rk[pygame.K_d]:
             self.xVelocity = self.velocity
-            
+
     def go_left(self):
         lk = pygame.key.get_pressed()
-        if lk = [pygame.K_a]:
+        if lk[pygame.K_a]:
             self.xVelocity = -self.velocity
-            
+
+    def stop(self):
+        """ Called when the user lets off the keyboard. """
+        self.change_x = 0
+
     def move(self):
         self.x += self.xVelocity
         # check x boundries
@@ -168,10 +177,12 @@ class player:
         self.size = (50, 50)
         rect = pygame.Rect((self.x, self.y), self.size)
         pygame.draw.rect(game_display, WHITE, rect)
-        pygame.display.update()
 
     def do(self):
-        self.keys()
+        self.go_up()
+        self.go_left()
+        self.go_right()
+        self.stop()
         self.move()
         self.draw()
 
@@ -180,48 +191,55 @@ P.setLocation(HEIGHT_WIDTH, 0)
 
 
 
-# main code
 def main():
-    game_display.blit() # not sure what arguments should be...
+    """ Main Program """
+
     pygame.display.flip()
     timer.tick(frames_per_second)
-    game_display.fill.(BLUE) # Add this to "clear" the screen.
-    
+    game_display.fill(BLUE)
+
     for f in range(math.floor(gettopcorner()[0]), math.floor(gettopcorner()[0]) * -1):
         terrainforelement = terrainnoise.noise2d(x = f / frequency, y = 0)
         for g in range(0, math.floor((terrainforelement + 1) * 10)):
             drawsquare(-f, -g + 58, GREEN)
-            
-            
-            
+
+
+
     # while loop must go inside main() function.
 
     done = False
     while not done:
+
+        pygame.display.flip()
+        timer.tick(frames_per_second)
+        game_display.fill(BLUE)
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 done = True
 
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_a:
-                    player.go_left()
+                    P.go_left()
                 if event.key == pygame.K_d:
-                    player.go_right()
-                if event.key == pygame.K_w:
-                    player.go_up()
+                    P.go_right()
+                if event.key == pygame.K_SPACE:
+                    P.go_up()
                 if event.key == pygame.K_q:
                     pygame.quit()
                     quit()
 
             if event.type == pygame.KEYUP:
-                if event.key == pygame.K_a and player.change_x < 0:
-                    player.stop()
-                if event.key == pygame.K_d and player.change_x > 0:
-                    player.stop()
-        
+                if event.key == pygame.K_a: # and P.change_x < 0:
+                    P.stop()
+                if event.key == pygame.K_d: #and P.change_x > 0:
+                    P.stop()
+                #if event.key == pygame.K_SPACE:
+                    #P.stop()
+
         pygame.display.flip()
         timer.tick(frames_per_second)
         P.do()
-        
+
 if __name__ == "__main__":
     main()
